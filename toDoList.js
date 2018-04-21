25d769f365c63be356f47570823351f280011939
Matt Crl <matth.carlevaris@gmail.com>
2018-04-21 13:12:37 +0200

    completed task function

--- toDoList.js
+++ toDoList.js
@@ -1,11 +1,26 @@
 $(document).ready(function(){
-    $("#add").on('click', addTask);
-});
 
-function addTask()
-{
-    let input = $("#todo");
-    let newListItem = input.val();
-    $("#todolist").append("<li><input class='selectComplete' type='checkbox'> - " + newListItem + " - <span class='glyphicon glyphicon-trash'></span> </li>");
-    input.val("");
-}
+    $("#add").on('click', addTask);
+    $("#todolist").on('change', '.selectComplete', completedTask);
+
+
+    function addTask()
+    {
+        let input = $("#todo");
+        let newListItem = input.val();
+        $("#todolist").append("<li><input class='selectComplete taskDone' type='checkbox'> - " + newListItem + " - <span class='glyphicon glyphicon-trash'></span> </li>");
+        input.val("");
+    }
+
+    function deleteTask()
+    {
+
+    }
+
+    function completedTask()
+    {
+        $(this).parent().toggleClass('taskDone');
+    }
+
+
+})
